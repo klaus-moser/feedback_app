a66fd78d0dcbeb13529287841ef8a62938539b10
klaus-moser <klaus-moser@posteo.de>
2022-04-25 22:55:47 +0200

    Reset

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
 function App() {
-
-  const title = 'Hello World'
-  const body = 'This is the body'
-  const comments = [
-    {id: 1, text: 'Comment one'},
-    {id: 2, text: 'Comment two'},
-    {id: 3, text: 'Comment three'},
-  ]
-
-  const loading = false
-  const showComments = true
-
-  if (loading) {
-    return <h1>Loading...</h1>
-  }
-
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  )
-
-
   return (
     <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
+      <h1>Hello World</h1>
     </div>
   );
 }
 
-export default App
+export default App;
