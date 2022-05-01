de3d741742b7dfaa84af3933a97efb57e9547ee5
klaus-moser <klaus-moser@posteo.de>
2022-05-01 20:18:26 +0200

    First component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <>
-      <Header />
+    <> 
+      <Header text="Feedback App"/>
       <div className="container">
         <h1>Hello World</h1>
       </div>
