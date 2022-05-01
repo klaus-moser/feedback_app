870cfa05e8cb73016f553c7105b27fd068b5e783
klaus-moser <klaus-moser@posteo.de>
2022-05-01 20:51:13 +0200

    Add styles to the header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 function App() {
   return (
     <> 
-      <Header text="Feedback App"/>
+      <Header text="Feedback UI"/>
       <div className="container">
         <h1>Hello World</h1>
       </div>
