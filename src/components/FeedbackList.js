d3b423de07a5d68f546bc6ac231d7d4da4c3a1b4
klaus-moser <klaus-moser@posteo.de>
2022-05-14 15:09:56 +0200

    Add packages + handleDelete

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,6 +1,7 @@
+import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
-function FeedbackList({feedback}) {
+function FeedbackList({feedback, handleDelete}) {
 
   if(!feedback || feedback.length === 0) {
     return <p>No Feedback yet!</p>
@@ -9,10 +10,21 @@ function FeedbackList({feedback}) {
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem 
+          key={item.id}
+          item={item}
+          handleDelete={handleDelete}/>
       ))}
     </div>
   )
+}
+
+FeedbackItem.propTypes = {
+  feedback: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }))
 }
 
 export default FeedbackList
